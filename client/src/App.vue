4a09a4a8c628b39d706400b9a908a680fec32e10
62312750 <naphatsornw62@nu.ac.th>
2021-10-01 00:56:00 +0700

    update

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+
+    <back-header/>
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: auto;
+  color: #2c3e50;
+  margin-top: 60px auto auto auto;
+  line-height: 40px;
+  font-size:18px;
+}
+/*Fade*/
+.fade-enter, .fade-leave-to{
+  opacity: 0;
+}
+.fade-emter-active, .fade-leave-active{
+  transition: opacity 0.5s;
+}
+.fade-enter-to{
+  opacity: 1;
+}
+</style>
